3bc7281bb160ddf167001cd38d31acbb3e749bbd
Allen Cohen <azc666@gmail.com>
2023-04-15 13:02:42 -0400

    test5 template height

--- resources/views/pdf-view.blade.php
+++ resources/views/pdf-view.blade.php
@@ -26,7 +26,7 @@
         session('prod_layout') === 'ABC' ||
         session('prod_layout') === 'PBC')
         <img src="https://azc666.com/assets/mpdf/bc_template_withText.jpg" style="width:100%; box-shadow: 0px 0px 10px;"
-            class="" style="margin-top: 80px; width:100%">
+            class="" style="margin-top: 70px; width:100%">
         @include('layouts/bc-cart-proof-layout')
         @endif
 
