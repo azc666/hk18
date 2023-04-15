2278b2709ab58855d0e9d68760684892bd1db682
Allen Cohen <azc666@gmail.com>
2023-04-15 12:56:48 -0400

    test3 hight adjust in pdf-view

--- resources/views/pdf-view.blade.php
+++ resources/views/pdf-view.blade.php
@@ -26,7 +26,7 @@
         session('prod_layout') === 'ABC' ||
         session('prod_layout') === 'PBC')
         <img src="https://azc666.com/assets/mpdf/bc_template_withText.jpg" style="width:100%; box-shadow: 0px 0px 10px;"
-            class="absolute top-20" style="width:100%">
+            class="pt-[80px]" style="width:100%">
         @include('layouts/bc-cart-proof-layout')
         @endif
 
