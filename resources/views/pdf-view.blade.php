c00204b8bf61b439e43138fe0f5e4002dac99cad
Allen Cohen <azc666@gmail.com>
2023-04-15 13:22:10 -0400

    revert pdf-view

--- resources/views/pdf-view.blade.php
+++ resources/views/pdf-view.blade.php
@@ -25,9 +25,8 @@
         session('prod_layout') === 'SBC' ||
         session('prod_layout') === 'ABC' ||
         session('prod_layout') === 'PBC')
-        <img src="https://azc666.com/assets/mpdf/bc_template_withText.jpg"
-            style="width:100%; box-shadow: 0px 0px 10px; margin-top: 70px; width:100%">
-        {{-- class="" style="margin-top: 70px; width:100%"> --}}
+        <img src="https://azc666.com/assets/mpdf/bc_template_withText.jpg" style="width:100%; box-shadow: 0px 0px 10px;"
+            class="absolute top-20" style="width:100%">
         @include('layouts/bc-cart-proof-layout')
         @endif
 
