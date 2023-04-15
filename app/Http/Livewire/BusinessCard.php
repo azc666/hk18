add3c3d79cbfc7ea0c0fed8bb8abc12710175eec
Allen Cohen <azc666@gmail.com>
2023-04-14 20:22:24 -0400

    test set paper BusinessCard fo bc

--- app/Http/Livewire/BusinessCard.php
+++ app/Http/Livewire/BusinessCard.php
@@ -448,7 +448,9 @@ class BusinessCard extends Component
 
         if ($product->id === 101 || $product->id === 102 || $product->id === 103) {
             $pdf = '';
-            $pdf = \Barryvdh\DomPDF\Facade\Pdf::loadview('pdf-view', $data)->setPaper([0, 0, 1220, 770]);
+            // $pdf = \Barryvdh\DomPDF\Facade\Pdf::loadview('pdf-view', $data)->setPaper([0, 0, 1220, 770]);
+            $pdf = \Barryvdh\DomPDF\Facade\Pdf::loadview('pdf-view', $data)->setPaper('a4', 'landscape');
+
         }
 
         if ($product->id === 104 || $product->id === 105 || $product->id === 106) {
