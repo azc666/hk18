51b14cf696dd33d4fcbab3bf884379a941d6227c
Allen Cohen <azc666@gmail.com>
2023-05-09 15:19:48 -0400

    update bc with correct color and font

--- resources/views/layouts/bc-cart-proof-layout.orig.blade.php
+++ resources/views/layouts/bc-cart-proof-layout.orig.blade.php
@@ -1,11 +1,4 @@
 <style>
-	@font-face {
-		font-family: 'Herculanum';
-		src: url('/fonts/Herculanum.ttf') format('ttf');
-		font-weight: normal;
-		font-style: normal;
-
-	}
 
 	@font-face {
 		font-family: "HelveticaNeueLTStd-LT";
@@ -18,15 +11,11 @@
 	}
 
 	.name-bc {
-		color: red;
-		/* color: #0b0b0b; */
+		/* color: red; */
+		color: #0b0b0b;
 		position: absolute;
 		font-size: 70px;
-		/* font-weight: 700; */
-		/* font-family: HelveticaNeueLtStd-Md, sans-serif; */
 		font-family: 'CustomFont', '"HelveticaNeueLTStd-MD"', sans-serif;
-		/* font-weight: 600; */
-		/* font-style: normal; */
 		letter-spacing: -2.5px;
 		/* top: 260px; */
 		top: 400px;
@@ -35,30 +24,22 @@
 	}
 
 	.title-bc {
-		color: red;
-		/* color: #00478F; */
+		/* color: red; */
+		color: #00478F;
 		position: absolute;
-		/* font-family: "HelveticaNeueLtStd Md"; */
 		font-family: 'CustomFont', '"HelveticaNeueLTStd-LT"', sans-serif;
-		/* font-weight: normal;
-		font-style: normal; */
 		letter-spacing: .075px;
 		font-size: 47.5px;
-		/* font-weight: lighter; */
 		top: 370px;
 		left: 112px;
 	}
 
 	.email-bc {
-		color: red;
-		/* color: #00478F; */
+		/* color: red; */
+		color: #00478F;
 		position: absolute;
 		font-size: 47.5px;
-		/* font-weight: 700; */
-		/* font-family: Helvetica, sans-serif; */
 		font-family: 'CustomFont', '"HelveticaNeueLTStd-MD"', sans-serif;
-		/* font-weight: 700; */
-		/* font-style: normal; */
 		letter-spacing: -.45px;
 		top: 740px;
 		right: 108.5px;
@@ -68,10 +49,8 @@
 		color: red;
 		/* color: #00478F; */
 		position: absolute;
-		/* font-family: Helvetica; */
 		font-family: 'CustomFont', '"HelveticaNeueLTStd-LT"', sans-serif;
 		font-size: 48.5px;
-		/* font-weight: lighter; */
 		letter-spacing: -0.15px;
 		top: 690px;
 		right: 109px;
@@ -81,10 +60,8 @@
 		color: red;
 		/* color: #00478F; */
 		position: absolute;
-		/* font-family: Helvetica; */
 		font-family: 'CustomFont', '"HelveticaNeueLTStd-LT"', sans-serif;
 		font-size: 48.5px;
-		/* font-weight: lighter; */
 		letter-spacing: -0.3px;
 		top: 628px;
 		right: 109px;
@@ -94,10 +71,8 @@
 		color: red;
 		/* color: #00478F; */
 		position: absolute;
-		/* font-family: Helvetica; */
 		font-family: 'CustomFont', '"HelveticaNeueLTStd-LT"', sans-serif;
 		font-size: 48px;
-		/* font-weight: lighter; */
 		letter-spacing: -.2px;
 		top: 570px;
 		right: 109px;
@@ -107,10 +82,8 @@
 		color: red;
 		/* color: #00478F; */
 		position: absolute;
-		/* font-family: Helvetica; */
 		font-family: 'CustomFont', '"HelveticaNeueLTStd-LT"', sans-serif;
 		font-size: 48px;
-		/* font-weight: lighter; */
 		letter-spacing: -0.5px;
 		top: 510px;
 		right: 109px;
@@ -122,30 +95,22 @@
 		position: absolute;
 		font-family: 'CustomFont', '"HelveticaNeueLTStd-LT"', sans-serif;
 		font-size: 49.5px;
-		/* font-weight: lighter; */
 		letter-spacing: -1px;
 		top: 450px;
 		right: 109px;
 	}
 </style>
-{{-- @dd(session('bc_name')) --}}
-<div>
 
-	{{-- <div id="bc_name" name="bc_name" class="absolute"> --}}
+	<div>
+
 		<div class="name-bc">
-			{{-- <div> --}}
 				{{ session('bc_name') }}
-			</div>
-			{{--
-		</div> --}}
+		</div>
 
-		{{-- <div id="bc_title" name="bc_title" class="absolute"> --}}
-			<div class="title-bc">
+		<div class="title-bc">
 				{{ session('bc_title') == 'Staff Attorney (Title will be updated to "Attorney")' ? 'Attorney' :
 				session('bc_title') }}
-			</div>
-			{{--
-		</div> --}}
+		</div>
 
 		<div class="email-bc">
 			{{ session('bc_email') }}
@@ -174,13 +139,6 @@
 		<div class="address1-bc">
 			{{ session('bc_address2') ? session('bc_address1') : session('HKName') }}
 		</div>
-
-		{{-- <div class="absolute">
-			<div
-				class="absolute top-[248px] -right-[526.5px] -mr-2 w-100 font-hellt tracking-[-0.037em] text-right text-[1.24rem] text-red-500 z-20">
-				{{ session('bc_address1') }}
-			</div>
-		</div> --}}
 
 		<div class="HKName-bc">
 			{{ session('bc_address2') ? session('HKName') : '' }}
