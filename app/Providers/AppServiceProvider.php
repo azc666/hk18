5ee1ae4ae1067d74b4a1d0fbdcf3c780f9484482
Allen Cohen <azc666@gmail.com>
2023-05-20 20:21:20 -0400

    remove dropbox from AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,16 +25,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Storage::extend('dropbox', function (Application $app, array $config) {
-            $adapter = new DropboxAdapter(new DropboxClient(
-                $config['authorization_token']
-            ));
+        // Storage::extend('dropbox', function (Application $app, array $config) {
+        //     $adapter = new DropboxAdapter(new DropboxClient(
+        //         $config['authorization_token']
+        //     ));
 
-            return new FilesystemAdapter(
-                new Filesystem($adapter, $config),
-                $adapter,
-                $config
-            );
-        });
+        //     return new FilesystemAdapter(
+        //         new Filesystem($adapter, $config),
+        //         $adapter,
+        //         $config
+        //     );
+        // });
     }
 }
